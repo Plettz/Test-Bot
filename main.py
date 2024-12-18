43c5e6a8dda5ada61942379d0b45dbcb9ae1ed10
Noah Beaudoin <noahgames@taconictech.com>
2024-12-17 21:46:31 -0500

    Created two new events: - Created a on_member_join() event. This event triggers whenever a member joins the server. First. the function recognizes the channel using the channel ID I provided. next, the bot sends a message in both the channel and a direct message to the user that just joined. Created a on_member_leave() event. This event triggers whenever a user leaves the server. The contents of this function are very similar to the on_member_join() but doesn't send the user a direct message.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 
 file = open("key.txt", 'r')
 TOKEN = file.read()
+print(TOKEN)
 
 client = commands.Bot(command_prefix='?', intents=discord.Intents.all())
 
@@ -15,5 +16,16 @@ async def on_ready():
 @client.command()
 async def hello(ctx):
     await ctx.send("hello")
+
+@client.event
+async def on_member_join(member):
+    channel = client.get_channel(1318764902327914498)
+    await member.send("Welcome") # Sends the user a direct message
+    await channel.send(f"Welcome {member.mention}") # Sends a message in the specified channel
+
+@client.event
+async def on_member_remove(member):
+    channel = client.get_channel(1318764929066467368)
+    await channel.send("Goodbye")
 
 client.run(TOKEN)
