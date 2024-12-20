1ea095ad3af108bc3ec250fa7bdbdd8bfe4805c9
Noah Beaudoin <noahgames@taconictech.com>
2024-12-20 14:50:27 -0500

    - Updated join, knock, and leave to be slash commands - Created the command media to send a video

--- main.py
+++ main.py
@@ -10,9 +10,15 @@ client = commands.Bot(command_prefix='?', intents=discord.Intents.all())
 
 @client.event
 async def on_ready():
-    print("-------------")
-    print("    ready    ")
-    print("-------------")
+    print("-------------------------------------")
+    print(f"Logged in as {client.user}")
+    print("-------------------------------------")
+
+    try:
+        synced = await client.tree.sync()
+        print(f"Synced {len(synced)} commands")
+    except Exception as e:
+        print(f"Failed to sync commands: {e}")
 
 @client.command()
 async def hello(ctx):
@@ -29,28 +35,35 @@ async def on_member_remove(member):
     channel = client.get_channel(1318764929066467368)
     await channel.send("Goodbye")
 
-@client.command(pass_context = True)
-async def join(ctx):
-    if (ctx.author.voice):
-        channel = ctx.author.voice.channel
+@client.tree.command(name="join", description="Join's the voice channel that the user is in")
+async def join(interaction: discord.Interaction):
+    if (interaction.user.voice):
+        channel = interaction.user.voice.channel
         await channel.connect()
+        await interaction.response.send_message("Joined voice channel")
     else:
-        await ctx.send("You are not in a channel, please join a voice channel and try again")
+        await interaction.response.send_message("You are not in a channel, please join a voice channel and try again")
 
-@client.command()
-async def knock(ctx):
-    if (ctx.voice_client):
-        voice = ctx.voice_client
+@client.tree.command(name="knock", description="Play's a knocking sound in a voice channel")
+async def knock(interaction: discord.Interaction):
+    if (interaction.guild.voice_client):
+        voice = interaction.guild.voice_client
         source = FFmpegPCMAudio("knocking-on-door.mp3")
+        interaction.response.send_message("Playing sound")
         player = voice.play(source)
     else:
-        await ctx.send("Could not play sound, need to be in a voice channel")
+        await interaction.response.send_message("Could not play sound, need to be in a voice channel")
 
-@client.command(pass_context = True)
-async def leave(ctx):
-    if (ctx.voice_client):
-        await ctx.guild.voice_client.disconnect()
+@client.tree.command(name="leave", description="Leave's connected voice channel")
+async def leave(interaction: discord.Interaction):
+    if (interaction.guild.voice_client):
+        await interaction.guild.voice_client.disconnect()
+        await interaction.response.send_message("Disconnected from the voice channel")
     else:
-        await ctx.send("I am not in a voice channel")
+        await interaction.response.send_message("I am not in a voice channel")
+
+@client.tree.command(name="media", description="Send's a video")
+async def media(interaction: discord.Interaction):
+    await interaction.response.send_message(file=discord.File("Zander.mov"))
 
 client.run(TOKEN)
